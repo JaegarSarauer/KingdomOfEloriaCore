2990c0cc727bc842fdfcd7ae744e406056b8bc70
Carson Roscoe <CarsonRoscoe7@gmail.com>
2020-05-30 18:24:40 -0700

    Migrating clothing

--- AccountVersion.js
+++ AccountVersion.js
@@ -1,34 +1,58 @@
 // NOTE: upgrade for a ID means upgrading to that ID
 const AccountVersion = [{
     id: 0, //versionID
-    upgrade: (userDef) => userDef // all accounts start at zero
+    upgradeData: (userDef) => userDef, // all accounts start at zero
+    onUpgradeComplete: (userDef)  => userDef,
 }, {
     id: 1,
-    upgrade: (userDef) => {
+    upgradeData: (userDef)  => userDef,
+    onUpgradeComplete: (userDef) => {
         // Upgrading from 0 to 1 means upgrading pre-customization. Force a customization
         
+        let shirtIdsToMigrate = [];
+        for(let i = 331; i <= 473; i += 2 ) {
+            shirtIdsToMigrate.push(i);
+        }
+        let pantIdsToMigrate = [];
+        for(let i = 475; i <= 491; i += 2 ) {
+            pantIdsToMigrate.push(i);
+        }
+
         const obInterfaces = [0, 5, 9, 10, 16, 17, 21, 22, 27];
         userDef.forAllAdventurers((adv) => {
+            // Migrate colored shirts from armor chest layer to clothing chest layer
+            if (adv.equipment.equipmentSlots[3] != null && shirtIdsToMigrate.includes(adv.equipment.equipmentSlots[3].id)) {
+                adv.equipment.equipmentSlots[9] = adv.equipment.equipmentSlots[3];
+                adv.equipment.equipmentSlots[3] = null;
+            }
+
+            // Migrate colored pants from armor pants layer to clothing pants layer
+            if (adv.equipment.equipmentSlots[4] != null && pantIdsToMigrate.includes(adv.equipment.equipmentSlots[4].id)) {
+                adv.equipment.equipmentSlots[10] = adv.equipment.equipmentSlots[4];
+                adv.equipment.equipmentSlots[4] = null;
+            }
+
+            // Customize adventurer UI
             let tutorialState = adv.state.getState(4);
-            if (tutorialState == 0) {
+            if (tutorialState != 1) {
                 adv.appearanceState.setEntityAtSlot(1);
                 adv.interfaces.enableInterfaces(obInterfaces);
             }
         })
-    }
+    },
 }];
 
 const upgradeAccount = (userDef) => {
-    let upgraded = false;
+    let upgradesCompleted = [];
     for (let i = userDef.accountVersion, le = AccountVersion.length; i < le; ++i) {
         let version = AccountVersion[i];
         if (version.id > userDef.accountVersion) {
             userDef.accountVersion = version.id;
-            upgraded = true;
-            version.upgrade(userDef);
+            version.upgradeData(userDef);
+            upgradesCompleted.push(version);
         }
     }
-    return upgraded;
+    return upgradesCompleted;
 }
 
 const LatestVersion = AccountVersion.length - 1;
