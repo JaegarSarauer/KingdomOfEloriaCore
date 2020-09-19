463a6fb5dfe779797f7cbb29ce4977af99b37cf3
Carson Roscoe <CarsonRoscoe7@gmail.com>
2020-09-19 11:34:22 -0700

    Tours travel and hold time

--- Tours.js
+++ Tours.js
@@ -1,41 +1,55 @@
+module.exports.TravelTime = 2000; // ms travel time for tours
+
 module.exports.Tours = Tours = [
     [{
         position: {x : 73, y : 360},
         text: 'The Teragon Guild Hall',
+        hold: 2000,
     }, {
         position: {x : 67, y : 360},
         text: 'The Teragon Community Chest',
+        hold: 2000,
     }, {
         position: {x : 65, y : 344},
         text: 'A bank',
+        hold: 2000,
     }, {
         position: {x : 40, y : 345},
         text: 'These are the shops in our guild',
+        hold: 2000,
     },],
     [{
         position: {x : 148, y : 15},
         text: 'The Salmo Guild Hall',
+        hold: 2000,
     }, {
         position: {x : 146, y : 12},
         text: 'The Teragon Community Chest',
+        hold: 2000,
     }, {
         position: {x : 140, y : 7},
         text: 'A bank',
+        hold: 2000,
     }, {
         position: {x : 160, y : 12},
         text: 'These are the shops in our guild',
+        hold: 2000,
     },],
     [{
         position: {x : 340, y : 43},
         text: 'The Teragon Guild Hall',
+        hold: 2000,
     }, {
         position: {x : 335, y : 47},
         text: 'The Teragon Community Chest',
+        hold: 2000,
     }, {
         position: {x : 353, y : 58},
         text: 'A bank',
+        hold: 2000,
     }, {
         position: {x : 340, y : 70},
         text: 'These are the shops in our guild',
+        hold: 2000,
     },]
 ];
