13ca99a0ef6ddafe8db9a34075d644f2c9a7fbcf
CarsonRoscoe <carsonroscoe7@gmail.com>
2020-10-23 16:15:53 -0400

    Realligned Skeleton mask

--- Item.js
+++ Item.js
@@ -3881,7 +3881,7 @@ const Item = Object.freeze([
     Get.Item.CosmeticHat(1087, 175, 'Goblin Mask', 'A spooky halloween goblin mask', 773, 0.4 ),
     Get.Item.CosmeticHat(1088, 176, 'Zombie Mask', 'A spooky halloween zombie mask', 774, 0.4 ),
     Get.Item.CosmeticHat(1089, 177, 'Ghoul Mask', 'A spooky halloween ghoul mask', 775, 0.4 ),
-    Get.Item.CosmeticHat(1090, 178, 'Skeleton Mask', 'A spooky halloween skeleton mask', 776, 0.4 ),
+    Get.Item.CosmeticHat(1090, 178, 'Skeleton Mask', 'A spooky halloween skeleton mask', 776, 0.33 ),
     Get.Item.CosmeticHat(1091, 179, 'Fright Hat', 'A spooky halloween fright mask', 777, 0.4 ),
     Get.Item.CosmeticHat(1092, 180, 'Demon Mask', 'A spooky halloween demon mask', 778, 0.55 ),
     Get.Item.CosmeticHat(1093, 181, 'Mummy Mask', 'A spooky halloween mummy mask', 779, 0.4 ),
