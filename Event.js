756ed4eb40d3bf173468998bb802b51a085c1da1
Jaegar Sarauer <jaegars@hotmail.com>
2020-11-21 16:44:30 -0800

    Added random events, fixed golem events.

--- Event.js
+++ Event.js
@@ -2,6 +2,7 @@ const Character = require('../typedef/Character');
 const NPCDef = require('../def/NPCDef');
 const Bounds = require('../def/Bounds').Bounds;
 const KingdomOfEloria = require('../KingdomOfEloria');
+const { AttackStyle, CombatStyle } = require('../internal/Combat');
 
 const startGolemEvent = (currentMap, targetEntity, golemCharID) => {
     let golem = new NPCDef.NPCDef(golemCharID, targetEntity.x, targetEntity.y, new Bounds(
@@ -16,18 +17,17 @@ const startGolemEvent = (currentMap, targetEntity, golemCharID) => {
         if (i == 9) {
             i = 11; //hitpoints
         }
-        if (i == 0) {
-            skillArray.push([0, 1000]);
-        } else if (i == 1 || i == 4 || i == 7) {
-            skillArray.push([i, 0.5]);
-        } else {
-            skillArray.push([i, golem.def.levelMultiplier * targetEntity.stats.getBaseLevel(0)]);
-        }
+        skillArray.push([i, golem.def.levelMultiplier * targetEntity.stats.getBaseLevel(i)]);
     }
+    skillArray[0][1] = golem.def.levelMultiplier * targetEntity.stats.getBaseLevel(2) * 10;
+    skillArray[1][1] = 4;
+
     golem.stats.fromSkillArray(skillArray);
 
     let tryDespawn = () => {// Mobile release fix (.default.)
-        if (golem.ownerUID != targetEntity.uid || golem.timers.getTimerTick(6) < KingdomOfEloria.i.currentTick) {
+        if (golem.ownerUID != targetEntity.uid 
+            || (golem.timers.getTimerTick(6) < KingdomOfEloria.i.currentTick
+            && golem.timers.getTimerTick(7) < KingdomOfEloria.i.currentTick)) {
             currentMap.despawnEntity(golem);
         } else {
             golem.timers.setTimer(2, 40, tryDespawn);
@@ -48,6 +48,101 @@ const startGolemEvent = (currentMap, targetEntity, golemCharID) => {
         attackTarget.performAction(golem);
 
     golem.timers.setTimer(2, 20, tryDespawn);
+    return true;
+}
+
+const startWasp = (currentMap, targetEntity) => {
+    let wasp = new NPCDef.NPCDef(130, targetEntity.x, targetEntity.y, new Bounds(
+        targetEntity.x - 2000,
+        targetEntity.y - 2000,
+        targetEntity.x + 2000,
+        targetEntity.y + 2000
+    ), false);
+
+    wasp.isRunning = true;
+    wasp.runEnergy = 9999999;
+
+    let playerHP = targetEntity.stats.getBaseLevel(11);
+    let playerDef = targetEntity.stats.getBaseLevel(2);
+    let skillArray = [[11, Math.floor(playerHP / 2)], [2, playerDef]]; //Really weak
+    wasp.stats.fromSkillArray(skillArray);
+
+    let tryDespawn = () => {
+        if (wasp.ownerUID != targetEntity.uid || targetEntity.mapID != wasp.mapID) {
+            currentMap.despawnEntity(wasp);
+            return;
+        }
+        if (wasp.timers.getTimerTick(6) < KingdomOfEloria.i.currentTick
+            && wasp.timers.getTimerTick(7) < KingdomOfEloria.i.currentTick) {
+                wasp.setPosition(targetEntity.lastX, targetEntity.lastY);
+        } 
+        wasp.timers.setTimer(2, 40, tryDespawn);
+    };
+
+
+    let spawned = currentMap.spawnEntity(wasp);
+    if (!spawned || !wasp.timers)
+        return false;
+
+    wasp.targetDef = targetEntity;
+    wasp.targetRefID = targetEntity.refID;
+    wasp.ownerUID = targetEntity.uid;
+
+    let attackTarget = targetEntity.getAction(6);
+    if (attackTarget)
+        attackTarget.performAction(wasp);
+
+    wasp.timers.setTimer(2, 20, tryDespawn);
+    return true;
+}
+
+const startHauntedGhost = (currentMap, targetEntity, boundsEntity) => {
+    let ghost = new NPCDef.NPCDef(29, boundsEntity.x, boundsEntity.y, new Bounds(
+        boundsEntity.x - 15,
+        boundsEntity.y - 15,
+        boundsEntity.x + 15,
+        boundsEntity.y + 15
+    ), false);
+
+    ghost.isMultiAttacker = true;
+
+    let skillArray = [];
+    for (let i = 0; i < 10; ++i) {
+        if (i == 9) {
+            i = 11; //hitpoints
+        }
+        skillArray.push([i, targetEntity.stats.getBaseLevel(i)]);
+    }
+    skillArray[0][1] = targetEntity.stats.getBaseLevel(2) * 4;
+    skillArray[1][1] = 1;
+
+    ghost.stats.fromSkillArray(skillArray);
+
+    let tryDespawn = () => {// Mobile release fix (.default.)
+        if (ghost.ownerUID != targetEntity.uid 
+            || (ghost.timers.getTimerTick(6) < KingdomOfEloria.i.currentTick
+            && ghost.timers.getTimerTick(7) < KingdomOfEloria.i.currentTick)) {
+            currentMap.despawnEntity(ghost);
+        } else {
+            startHauntedGhost(currentMap, targetEntity, ghost);
+            ghost.timers.setTimer(2, 40, tryDespawn);
+        }
+    };
+
+
+    let spawned = currentMap.spawnEntity(ghost);
+    if (!spawned || !ghost.timers)
+        return false;
+
+    ghost.targetDef = targetEntity;
+    ghost.targetRefID = targetEntity.refID;
+    ghost.ownerUID = targetEntity.uid;
+
+    let attackTarget = targetEntity.getAction(6);
+    if (attackTarget)
+        attackTarget.performAction(ghost);
+
+    ghost.timers.setTimer(2, 20, tryDespawn);
     return true;
 }
 
@@ -85,5 +180,15 @@ module.exports.Event = [{
     id: 6,
     startEvent: (currentMap, targetEntity) => {
         return startGolemEvent(currentMap, targetEntity, 58);
+    }
+}, {
+    id: 7,
+    startEvent: (currentMap, targetEntity) => {
+        return startHauntedGhost(currentMap, targetEntity, targetEntity);
+    }
+}, {
+    id: 8,
+    startEvent: (currentMap, targetEntity) => {
+        return startWasp(currentMap, targetEntity);
     }
 }];
