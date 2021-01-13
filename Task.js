cc969ccf3521401fb700de1a4d1c4ecc2bce5e47
Jaegar Sarauer <jaegars@hotmail.com>
2021-01-12 16:25:26 -0800

    Task template for task system

---
+++ Task.js
@@ -0,0 +1,23 @@
+// Task Template
+
+// class TaskType {
+//     constructor(taskTypeID,)
+// }
+
+// const TaskTypes = {
+//     KILL_X_NPC: TaskType(0, )
+// };
+
+// const Task = (id, taskTypeID, workPoints, pointsPerAction) => {
+//     return {
+//         id,
+//         workPoints,
+//         pointsPerAction,
+//     }
+// }
+
+// const TaskDictionary = {
+//     EASY: [
+//         Task(0, )
+//     ],
+// };
